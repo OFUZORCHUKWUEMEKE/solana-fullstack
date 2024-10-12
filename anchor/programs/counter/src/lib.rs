5118fe04ec56f29ff60fa16a3675b1c1d01da46a
Ofuzor chukwuemeke <uniben2018@gmail.com>
2024-10-12 15:33:48 +0100

    edited

--- anchor/programs/counter/src/lib.rs
+++ anchor/programs/counter/src/lib.rs
@@ -5,66 +5,112 @@ use anchor_lang::prelude::*;
 declare_id!("AsjZ3kWAUSQRNt2pZVeJkywhZ6gpLpHZmJjduPmKZDZZ");
 
 #[program]
-pub mod counter {
+pub mod voting {
     use super::*;
 
-  pub fn close(_ctx: Context<CloseCounter>) -> Result<()> {
-    Ok(())
-  }
+    pub fn initialize_poll(
+        ctx: Context<InitializePoll>,
+        poll_id: u64,
+        description: String,
+        poll_end: u64,
+        poll_start: u64,
+    ) -> Result<()> {
+        let poll = &mut ctx.accounts.poll;
+        poll.poll_id = poll_id;
+        poll.description = description;
+        poll.poll_end = poll_end;
+        poll.poll_start = poll_start;
+        poll.candidate_amount = 0;
 
-  pub fn decrement(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.counter.count = ctx.accounts.counter.count.checked_sub(1).unwrap();
-    Ok(())
-  }
+        Ok(());
+    }
 
-  pub fn increment(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.counter.count = ctx.accounts.counter.count.checked_add(1).unwrap();
-    Ok(())
-  }
+    pub fn initialize_candidate(
+        ctx: Context<InitializeCandidate>,
+        candidate_name: String,
+        _poll_id: u64,
+    ) -> Result<()> {
+        let candidate = &mut ctx.accounts.candidate;
+        let poll = &mut ctx.accounts.poll;
+        poll.candidate_amount += 1;
+        candidate.candidate_name = candidate_name;
+        candidate.candidate_votes = 0;
+        // candidate
+        Ok(())
+    }
 
-  pub fn initialize(_ctx: Context<InitializeCounter>) -> Result<()> {
-    Ok(())
-  }
-
-  pub fn set(ctx: Context<Update>, value: u8) -> Result<()> {
-    ctx.accounts.counter.count = value.clone();
-    Ok(())
-  }
+    pub fn vote(ctx: Context<Vote>, candidate_name: String, _poll_id: u64) -> Result<()> {
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
-pub struct InitializeCounter<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
-
-  #[account(
-  init,
-  space = 8 + Counter::INIT_SPACE,
-  payer = payer
+#[instruction(candidate_name:String,poll_id:u64)]
+pub struct Vote<'info> {
+    pub signer: Signer<'info>,
+    #[account(
+      mut,
+    seeds=[poll_id.to_le_bytes().as_ref()],
+    bump
   )]
-  pub counter: Account<'info, Counter>,
-  pub system_program: Program<'info, System>,
-}
-#[derive(Accounts)]
-pub struct CloseCounter<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
-
-  #[account(
-  mut,
-  close = payer, // close account and return lamports to payer
+    pub poll: Account<'info, Poll>,
+    #[account(
+      mut,
+    seeds=[candidate_name.as_bytes(),poll_id.to_le_bytes().as_ref()],
+    bump
   )]
-  pub counter: Account<'info, Counter>,
+    pub candidate: Account<'info, Candidate>,
 }
 
 #[derive(Accounts)]
-pub struct Update<'info> {
-  #[account(mut)]
-  pub counter: Account<'info, Counter>,
+#[instruction(poll_id:u64)]
+pub struct InitializePoll<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+    init,
+    payer=signer,
+    space = 8 + Poll::INIT_SPACE,
+    seeds=[poll_id.to_le_bytes().as_ref()],
+    bump
+  )]
+    pub poll: Account<'info, Poll>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(candidate_name:String,poll_id:u64)]
+pub struct InitializeCandidate<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(mut,seeds=[poll_id.to_le_bytes().as_ref()],bump)]
+    pub poll: Account<'info, Poll>,
+    #[account(
+    init,
+    payer=signer,
+    space = 8 + Candidate::INIT_SPACE,
+    seeds=[candidate_name.as_bytes(),poll_id.to_le_bytes().as_ref()],
+    bump
+  )]
+    pub candidate: Account<'info, Candidate>,
+    pub system_program: Program<'info, System>,
 }
 
 #[account]
 #[derive(InitSpace)]
-pub struct Counter {
-  count: u8,
+pub struct Candidate {
+    #[max_len(32)]
+    pub candidate_name: String,
+    pub candidate_votes: u64,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Poll {
+    pub poll_id: u64,
+    #[max_len(200)]
+    pub description: String,
+    pub poll_start: u64,
+    pub poll_end: u64,
+    pub candidate_amount: u64,
 }
